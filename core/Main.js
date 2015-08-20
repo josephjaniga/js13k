4cd81b030a6905dfa073e1371efd7cff1d4ff964
Joe Janiga <josephjaniga@gmail.com>
2015-08-19 22:32:17 -0400

    updated the build and configured the dev setup

--- core/Main.js
+++ core/Main.js
@@ -1,1 +1,4 @@
 var game = new Game();
+
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,23 @@
 'use strict';
 
-var gulp = require('gulp'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify'),
-    minifyHTML = require('gulp-minify-html'),
-    zip = require('gulp-zip'),
-    del = require('del'),
-    babel = require('gulp-babel'),
-    concat = require('gulp-concat'),
-    env = require('gulp-env'),
+var gulp        = require('gulp'),
+    rename      = require('gulp-rename'),
+    uglify      = require('gulp-uglify'),
+    minifyHTML  = require('gulp-minify-html'),
+    zip         = require('gulp-zip'),
+    del         = require('del'),
+    babel       = require('gulp-babel'),
+    concat      = require('gulp-concat'),
+    env         = require('gulp-env'),
+    watch       = require('gulp-watch'),
+    size        = require('gulp-size'),
+    notify      = require('gulp-notify'),
 
     // the filepath setup
     _DATESTAMP_     = Date.now(),
     CORE_SOURCE     = ['core/*.js'],
     CORE_DEST       = 'build/core/',
+    DEV_CORE_DEST   = 'core',
     INDEX_SOURCE    = 'index.html',
     INDEX_DEST      = 'build/',
     ZIP_SOURCE      = ['build/'],
@@ -27,13 +31,30 @@ gulp.task('set-env', function(){
 gulp.task('default', ['set-env', 'buildCompressed']);
 
 /**
- * BUILD
+ * BUILD DEVELOPMENT
+ * WATCH TASK
+ */
+gulp.task('watch', function(){
+    var TOTAL_SOURCE = [CORE_SOURCE[0], '!core/core.es5.min.js'];
+    watch(TOTAL_SOURCE, function(){
+        del("core/core.es5.min.js");
+        return gulp.src(TOTAL_SOURCE)
+            .pipe(babel())
+            .pipe(concat('core.es5.min.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest(DEV_CORE_DEST));
+    });
+});
+
+/**
+ * BUILD PRODUCTION
+ *  load environmental variables
  *  concatenate
  *  transpile ES6
  *  uglify - min.js
  *  minifyHTML
  */
-gulp.task('buildCompressed', ['clean', 'buildCore', 'buildIndex', 'zip']);
+gulp.task('buildCompressed', ['clean', 'buildCore', 'buildIndex', 'zip', 'zipSize']);
 
 gulp.task('clean', ['cleanZips', 'cleanBuild']);
 
@@ -54,17 +75,18 @@ gulp.task('buildIndex', ['clean'], function(cb) {
 gulp.task('buildCore', ['clean'], function(cb) {
     return gulp.src(CORE_SOURCE)
         .pipe(babel())
-        .pipe(concat('core.es5.js'))
+        .pipe(concat('core.es5.min.js'))
         .pipe(uglify())
-        .pipe(rename({extname:'.min.js'}))
         .pipe(gulp.dest(CORE_DEST));
 });
 
-/**
- * this is the zipped output of the build, content size to be measured for the compo under 13k
- */
-gulp.task('zip', ['buildCore', 'buildIndex'], function(cb) {
+gulp.task('zip', ['buildCore', 'buildIndex'], function() {
     return gulp.src(ZIP_SOURCE)
         .pipe(zip('build_'+_DATESTAMP_+'.zip'))
         .pipe(gulp.dest(ZIP_DEST));
+});
+
+gulp.task('zipSize', ['zip'],  function(){
+    return gulp.src('zip/build_'+_DATESTAMP_+'.zip')
+        .pipe(size({showFiles:true, pretty: true}));
 });
